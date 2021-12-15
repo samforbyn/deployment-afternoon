dafb1c86b03c8ff37eabd984239890db4abdaf41
Sam Forbyn <samforbyn@gmail.com>
2021-12-15 13:42:06 -0700

    added stylesheet

--- server/index.js
+++ server/index.js
@@ -7,8 +7,12 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../styles.css'))
+  })
+
 const port = process.env.PORT || 4006
 
 app.listen(port, () => {
     console.log(`beam me up, port ${port}`)
-})
+}) 
